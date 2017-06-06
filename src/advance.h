173e093455aa5f73da7cd076c31b3e880e477e41
Chun Shen <chunshen1987@gmail.com>
2017-06-06 18:05:43 -0400

    some update

--- src/advance.h
+++ src/advance.h
@@ -129,6 +129,7 @@ class Advance {
         double qi_nbr_y[][5], double qi_nbr_eta[][5],
         double qi_rk0[][5], double grid_array[][5], double *grid_array_temp);
 
+#pragma acc routine seq
     void prepare_vis_array(
         Field *hydro_fields, int rk_flag, int ieta, int ix, int iy,
         int n_cell_eta, int n_cell_x, int n_cell_y,
@@ -136,6 +137,7 @@ class Advance {
         double vis_nbr_x[][19], double vis_nbr_y[][19],
         double vis_nbr_eta[][19], InitData *DATA);
 
+#pragma acc routine seq
     void prepare_velocity_array(double tau_rk, Field *hydro_fields,
                                      int ieta, int ix, int iy, int rk_flag,
                                      int n_cell_eta, int n_cell_x,
@@ -144,6 +146,7 @@ class Advance {
                                      double vis_array_new[][19],
                                      double *grid_array_temp);
 
+#pragma acc routine seq
     int FirstRKStepW(double tau, int rk_flag, int n_cell_eta,
                      int n_cell_x, int n_cell_y, double vis_array[][19],
                      double vis_nbr_tau[][19], double vis_nbr_x[][19],
@@ -151,6 +154,7 @@ class Advance {
                      double velocity_array[][20], double grid_array[][5],
                      double vis_array_new[][19]);
 
+#pragma acc routine seq
     void MakeWSource(double tau, double qi_array[][5],
                      int n_cell_eta, int n_cell_x, int n_cell_y,
                      double vis_array[][19],
@@ -158,12 +162,14 @@ class Advance {
                      double vis_nbr_y[][19], double vis_nbr_eta[][19],
                      double qi_array_new[][5], InitData* DATA);
 
+#pragma acc routine seq
     int Make_uWRHS(double tau, int n_cell_eta, int n_cell_x, int n_cell_y,
                    double vis_array[][19], double vis_nbr_x[][19],
                    double vis_nbr_y[][19], double vis_nbr_eta[][19],
                    double velocity_array[][20],
                    double vis_array_new[][19]);
 
+#pragma acc routine seq
     double Make_uWSource(double tau, int n_cell_eta, int n_cell_x,
                          int n_cell_y, double vis_array[][19],
                          double velocity_array[][20],
@@ -177,9 +183,11 @@ class Advance {
     void update_grid_array_from_field(
                 Field *hydro_fields, int idx, double *grid_array, int rk_flag);
 
+#pragma acc routine seq
     void update_vis_array_from_field(Field *hydro_fields, int idx,
                                      double *vis_array, int rk_flag);
 
+#pragma acc routine seq
     void update_vis_prev_tau_from_field(Field *hydro_fields, int idx,
                                         double *vis_array,int rk_flag);
 
@@ -191,11 +199,14 @@ class Advance {
     void update_grid_cell(double grid_array[][5], Field *hydro_fields, int rk_flag,
                           int ieta, int ix, int iy,
                           int n_cell_eta, int n_cell_x, int n_cell_y);
+#pragma acc routine seq
     void update_grid_cell_viscous(double vis_array[][19], Field *hydro_fields,
                                   int rk_flag, int ieta, int ix, int iy,
                                   int n_cell_eta, int n_cell_x, int n_cell_y);
 
+#pragma acc routine seq
     int QuestRevert(double tau, double *vis_array, double *grid_array);
+#pragma acc routine seq
     int QuestRevert_qmu(double tau, double *vis_array, double *grid_array);
 
     //! This function computes the vector [T^\tau\mu, J^\tau] from the
