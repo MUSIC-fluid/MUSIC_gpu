14115580494a43839eb3aa538319ba738bb577ba
Chun Shen <chunshen1987@gmail.com>
2017-06-06 17:00:08 -0400

    add pragma to advance.h

--- src/advance.h
+++ src/advance.h
@@ -35,10 +35,13 @@ class Advance {
 
     int AdvanceIt(double tau_init, InitData *DATA, Field *hydro_fields,
                   int rk_flag);
-
+#pragma acc routine seq
     double get_pressure(double e_local, double rhob);
+#pragma acc routine seq
     double get_cs2(double e_local, double rhob);
+#pragma acc routine seq
     double p_e_func(double e_local, double rhob);
+#pragma acc routine seq
     double p_rho_func(double e_local, double rhob);
 
     //int FirstRKStepT(double tau, int rk_flag,
@@ -53,6 +56,7 @@ class Advance {
     //                 double *qimhL, double *qimhR,
     //                 double *grid_array_hL, double *grid_array_hR,
     //                 InitData* DATA);
+#pragma acc routine seq
     int FirstRKStepT(double tau, int rk_flag,
                           double qi_array[][5], double qi_nbr_x[][5],
                           double qi_nbr_y[][5], double qi_nbr_eta[][5],
@@ -67,6 +71,7 @@ class Advance {
                           InitData *DATA);
 
 
+#pragma acc routine seq
     void MakeDeltaQI(double tau, double qi_array[][5], double qi_nbr_x[][5],
                      double qi_nbr_y[][5], double qi_nbr_eta[][5],
                      int n_cell_eta, int n_cell_x, int n_cell_y,
@@ -76,25 +81,35 @@ class Advance {
                      double *grid_array_hL, double *grid_array_hR,
                      InitData* DATA);
     
+#pragma acc routine seq
     double MaxSpeed(double tau, int direc, double *grid_array);
     
+#pragma acc routine seq
     void revert_grid(double *grid_array, double *grid_prev);
+#pragma acc routine seq
     int ReconstIt_velocity_Newton(double *grid_array, double tau, double *uq,
                                   double *grid_array_p);
+#pragma acc routine seq
     double reconst_velocity_f(double v, double T00, double M,
                               double J0);
+#pragma acc routine seq
     double reconst_velocity_f_Newton(double v, double T00, double M,
                                      double J0);
+#pragma acc routine seq
     double reconst_velocity_df(double v, double T00, double M, double J0);
     
+#pragma acc routine seq
     double reconst_u0_f(double u0, double T00, double K00, double M,
                         double J0);
+#pragma acc routine seq
     double reconst_u0_f_Newton(double u0, double T00, double K00,
                                double M, double J0);
+#pragma acc routine seq
     double reconst_u0_df(double u0, double T00, double K00, double M,
                          double J0);
     
 
+#pragma acc routine seq
     double get_TJb_new(double *grid_array, int mu, int nu);
 
     
@@ -104,6 +119,7 @@ class Advance {
     //    int n_cell_eta, int n_cell_x, int n_cell_y, double **qi_array,
     //    double **qi_nbr_x, double **qi_nbr_y, double **qi_nbr_eta,
     //    double **qi_rk0, double **grid_array, double *grid_array_temp);
+#pragma acc routine seq
     void prepare_qi_array(
         double tau, Field *hydro_fields, int rk_flag, int ieta, int ix, int iy,
         int n_cell_eta, int n_cell_x, int n_cell_y,
@@ -129,6 +145,7 @@ class Advance {
                      double **vis_nbr_y, double **vis_nbr_eta,
                      double **velocity_array, double **grid_array,
                      double **vis_array_new);
+#pragma acc routine seq
     void update_grid_array_from_field(
                 Field *hydro_fields, int idx, double *grid_array, int rk_flag);
 
@@ -138,9 +155,11 @@ class Advance {
     void update_vis_prev_tau_from_field(Field *hydro_fields, int idx,
                                         double *vis_array,int rk_flag);
 
+#pragma acc routine seq
     void update_grid_array_to_hydro_fields(
             double *grid_array, Field *hydro_fields, int idx, int rk_flag);
 
+#pragma acc routine seq
     void update_grid_cell(double grid_array[][5], Field *hydro_fields, int rk_flag,
                           int ieta, int ix, int iy,
                           int n_cell_eta, int n_cell_x, int n_cell_y);
@@ -153,7 +172,9 @@ class Advance {
 
     //! This function computes the vector [T^\tau\mu, J^\tau] from the
     //! grid_array [e, v^i, rhob]
+#pragma acc routine seq
     void get_qmu_from_grid_array(double tau, double *qi, double *grid_array);
+#pragma acc routine seq
     double minmod_dx(double up1, double u, double um1);
 };
 
